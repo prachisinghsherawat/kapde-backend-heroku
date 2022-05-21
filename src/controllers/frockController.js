71d46f78a82274103069318966c7bc84abb389aa
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-21 14:15:43 +0530

    kapde-backend

---
+++ src/controllers/frockController.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+const router = express.Router();
+
+const Frock = require("../models/frockModel");
+
+router.post("/frock",async(req,res)=>{
+
+    try {
+        let frock = await Frock.create(req.body);
+        return res.status(200).send(frock)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+router.get("/frock",async(req,res)=>{
+
+    try {
+        let frock = await Frock.find().lean().exec();
+        return res.status(200).send(frock)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+router.patch("/frock/:id",async(req,res)=>{
+
+    try {
+        let frock = await Frock.findByIdAndUpdate(req.params.id , req.body ,{new : true}).lean().exec();
+        return res.status(200).send(frock)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+router.delete("/frock/:id",async(req,res)=>{
+
+    try {
+        let frock = await Frock.findByIdAndDelete(req.params.id).lean().exec();
+        return res.status(200).send(frock)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+module.exports = router;
---
+++ src/controllers/kurtiController.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+const router = express.Router();
+
+const Kurti = require("../models/kurtiModel");
+
+router.post("/kurti",async(req,res)=>{
+
+    try {
+        let kurti = await Kurti.create(req.body);
+        return res.status(200).send(kurti)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+router.get("/kurti",async(req,res)=>{
+
+    try {
+        let kurti = await Kurti.find().lean().exec();
+        return res.status(200).send(kurti)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+router.patch("/kurti/:id",async(req,res)=>{
+
+    try {
+        let kurti = await Kurti.findByIdAndUpdate(req.params.id , req.body ,{new : true}).lean().exec();
+        return res.status(200).send(kurti)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+router.delete("/kurti/:id",async(req,res)=>{
+
+    try {
+        let kurti = await Kurti.findByIdAndDelete(req.params.id).lean().exec();
+        return res.status(200).send(kurti)
+
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+})
+
+module.exports = router;
