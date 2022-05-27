21959bf8d8be79f657e56fe34044f569d7c1b318
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-27 23:10:31 +0530

    Denims Controller

--- src/controllers/denimController.js
+++ src/controllers/denimController.js
@@ -18,6 +18,7 @@ router.post("/denims",async(req,res)=>{
 router.get("/denims",async(req,res)=>{
 
     let page = req.query.page
+    
     try {
         let denim = await Denim.find().limit(6).skip((page-1)*6).lean().exec();
         return res.status(200).send(denim)
