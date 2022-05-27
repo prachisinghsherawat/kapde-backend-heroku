48ea62954f388b72c8528b1d95ef68d3ce425782
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-27 23:12:09 +0530

    Kurtis Controller

--- src/controllers/kurtiController.js
+++ src/controllers/kurtiController.js
@@ -18,6 +18,7 @@ router.post("/kurti",async(req,res)=>{
 router.get("/kurti",async(req,res)=>{
 
     let page = req.query.page
+    
     try {
         let kurti = await Kurti.find().limit(6).skip((page-1)*6).lean().exec();
         return res.status(200).send(kurti)
