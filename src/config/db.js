a4f37f2f0a9f3de1a7d91d1bd952d91e047a97ac
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-27 23:09:32 +0530

    config db

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const connect = () => {
 
     return mongoose.connect(
-       process.env.MONGODB_URL
+       process.env.MONGODB_URL                         
     )
 }
 
